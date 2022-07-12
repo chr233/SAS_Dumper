58fb5204b2ab5b25d61640f358fe64f740b27969
Chr_ <chr@chrxw.com>
2022-07-12 12:34:12 +0800

    版本更新

--- SAS_Dumper/AssemblyInfo.cs
+++ SAS_Dumper/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: System.CLSCompliant(false)]
-[assembly: AssemblyVersion("1.0.5.008")]
-[assembly: AssemblyFileVersion("1.0.5.008")]
+[assembly: AssemblyVersion("1.0.5.009")]
+[assembly: AssemblyFileVersion("1.0.5.009")]
 
 [assembly: AssemblyCopyright("Copyright © 2022 Chr_")]
 [assembly: AssemblyProduct("SAS_Dumper")]
